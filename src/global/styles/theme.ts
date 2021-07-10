14d9c3a772202602d72f7939251281c86651857b
matheus12344 <nationsworship5@gmail.com>
2021-07-09 23:27:04 -0300

    fonte dnv

---
+++ src/global/styles/theme.ts
@@ -0,0 +1,18 @@
+export const theme = {
+    colors:{
+        blue: "#2675EC",
+        white: "#FFFFFF",
+        black: "#131313",
+        gray: "#848484",
+        whiteSecondary: "#FAFAFA",
+        whiteTerceiro: "#F6F6F6",
+        green: "#40D81A",
+        red: "#FF0000",
+    },
+    
+    fonts: {
+        title700: 'Ubuntu_700Bold',
+        title500: 'Ubuntu_500Medium',
+        text300: 'Ubuntu_300Light'
+    }
+}
